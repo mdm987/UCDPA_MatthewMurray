e31f87c7d5ffba1a40a044ad8f2bcc55384468db
mdm987 <93350328+mdm987@users.noreply.github.com>
2022-01-09 18:48:40 +0000

    Commit Initial Test 29112021

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ athletes.sort_values('discipline', ascending=False)
 athletes_object = athletes['birth_date']
 print(athletes_object)
 
-# Replace Missing values - Where blank birth country replace with unknown
+# Replace Missing values - Where blank values replace with unknown
 athletes['birth_country'] = athletes['birth_country'].replace(np.nan, 'Unknown')
+medals['athlete_sex'] = medals['athlete_sex'].replace(np.nan, 'Unknown')
 
 # Slicing - Indexed Medals Above
 print(medals[33:39])
@@ -49,6 +50,11 @@ print(first, "\n\n\n", second)
 #olympicjoin_df = athletes.merge(medals, how = 'left', on = 'name')
 #print(olympicjoin_df)
 
+# Example custom function
+def multiplythreenums (a,b,c) :
+    return a * b * c
+print (multiplythreenums(3,3,3))
+print (multiplythreenums(9900,8800,7700))
 
 # View header shape datatype
 print(medals.head())
@@ -67,17 +73,44 @@ print(athletes['birth_country'])
 #medals.head()
 
 
-## Scatter Chart Example
-#plt.scatter(medals['event'], medals['country'], s=3)
-#plt.xlabel('Event')
-#plt.ylabel('Country')
-#plt.title('Olympic Medals')
+
+# 1 - Gender breakdown of competing athletes
+gender_breakdown = athletes.gender.value_counts()
+plt.figure(figsize=(12,8))
+plt.title('Gender breakdown of the athletes')
+plt.pie(gender_breakdown, labels=gender_breakdown.index,autopct='%1.1f%%',startangle=90)
 #plt.show()
 
-
-# Plot the top 5 countries with the most athletes competing in Japan 2020
+# 2 - Top 5 countries with the most athletes
 country_rep = athletes.country.value_counts().sort_values(ascending=False).head(5)
 plt.figure(figsize=(10,6))
 plt.title('Countries with most competing athletes (Top 5)')
 sns.barplot(x=country_rep.index,y=country_rep, palette = 'Set3')
-plt.show()
+plt.xlabel('Countries')
+plt.ylabel('Number of Athletes')
+#plt.show()
+
+# 3 - Age Breakdown across the athletes
+plt.figure(figsize=(12,8))
+plt.title('Age breakdown of the athletes')
+plt.xlabel('Age')
+plt.ylabel('Number of athletes')
+plt.hist(athletes.Age, bins = np.arange(12,70,2),color='pink',edgecolor ='black')
+# plt.show()
+
+# 4 - Top 10 countries with most medal wins
+event_medals = medals.country.value_counts().sort_values(ascending=False).head(8)
+plt.figure(figsize=(14,10))
+plt.title('Countries with the most medals (Top 10)')
+sns.barplot(x=event_medals,y=event_medals.index, palette = 'Set2')
+plt.xlabel('Number of medals')
+#plt.show()
+
+# 5 - Seaborn Scatter Plot showing Age by Olympic Discipline
+plt.figure(figsize=(16,12))
+sns.set_style("whitegrid")
+axis = sns.scatterplot (x="Age", y="discipline", data=athletes, hue='gender')
+plt.title ('Age by Olympic Discipline')
+#plt.show()
+
+
